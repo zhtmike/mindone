8c0d4fbaccf343ea62e03bb5a09514ed57bcd2b8
Mike Cheung <zhtmike@gmail.com>
2024-11-22 14:00:10 +0800

    support vae decode

---
+++ examples/opensora_hpcai/scripts/run/run_infer_cogvideox_5b_1_5_vae_decode.sh
@@ -0,0 +1,9 @@
+python scripts/infer_vae_decode.py \
+    --latent_folder samples/denoised_latents \
+    --output_path samples \
+    --vae_type CogVideoX-VAE \
+    --sd_scale_factor 0.7 \
+    --vae_checkpoint models/CogVideoX-1.5-5b/vae/diffusion_pytorch_model.safetensors \
+    --dtype fp32 \
+    --mode 1 \
+    --save_format gif
