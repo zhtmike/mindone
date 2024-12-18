59bb43d8067e53d729476b323eb762824d5471b8
Mike Cheung <zhtmike@gmail.com>
2024-12-18 10:10:04 +0800

    add recompute block number

--- examples/opensora_hpcai/scripts/run/run_train_cogvideox_5b_1_5_dp_adamw_8p.sh
+++ examples/opensora_hpcai/scripts/run/run_train_cogvideox_5b_1_5_dp_adamw_8p.sh
@@ -6,4 +6,5 @@ msrun --worker_num=8 --local_worker_num=8 --log_dir=$output_dir scripts/train.py
     --video_folder datasets/mixkit-100videos/mixkit \
     --text_embed_folder datasets/mixkit-100videos/t5_224 \
     --vae_latent_folder datasets/mixkit-100videos/vae_768_1360 \
-    --use_parallel True
+    --use_parallel True \
+    --num_recompute_blocks 38  # the smallest number of recompute blocks it can hold
