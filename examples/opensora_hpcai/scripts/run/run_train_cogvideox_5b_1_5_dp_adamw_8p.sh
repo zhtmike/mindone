218784adcc77664809c4ba12e47e1e562e95896f
Mike Cheung <zhtmike@gmail.com>
2025-01-03 17:57:47 +0800

    update comments

--- examples/opensora_hpcai/scripts/run/run_train_cogvideox_5b_1_5_dp_adamw_8p.sh
+++ examples/opensora_hpcai/scripts/run/run_train_cogvideox_5b_1_5_dp_adamw_8p.sh
@@ -7,4 +7,4 @@ msrun --worker_num=8 --local_worker_num=8 --log_dir=$output_dir scripts/train.py
     --text_embed_folder datasets/mixkit-100videos/t5_224 \
     --vae_latent_folder datasets/mixkit-100videos/vae_768_1360 \
     --use_parallel True \
-    --num_recompute_blocks 38  # the smallest number of recompute blocks it can hold
+    --num_recompute_blocks 38  # the smallest number of recompute blocks it can hold, 37 blocks for 77 frames
