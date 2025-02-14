74a960951c7530d177abcbbfd8e8c838add5d86d
Mike Cheung <zhtmike@gmail.com>
2025-02-14 14:09:04 +0800

    update script

--- examples/opensora_hpcai/scripts/run/run_train_cogvideox_5b_1_5_i2v_dp_adamw_8p.sh
+++
@@ -1,9 +0,0 @@
-output_dir=logs/cogvideox_5b_1_5_i2v_dp_adamw_8p
-
-msrun --worker_num=8 --local_worker_num=8 --log_dir=$output_dir scripts/train.py \
-    --config configs/cogvideox_5b-v1-5/train/train_i2v.yaml \
-    --csv_path datasets/mixkit-100videos/video_caption_train.csv \
-    --video_folder datasets/mixkit-100videos/mixkit \
-    --text_embed_folder datasets/mixkit-100videos/t5_224 \
-    --use_parallel True \
-    --num_frames 45
